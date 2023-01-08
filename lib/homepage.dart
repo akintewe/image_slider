861ac2cbc04e5db80709c3eb86c9a37e3e211a53
Akintewe Oluwasola <nathan@Akintewes-MacBook-Pro.local>
2023-01-08 10:42:50 +0100

    created and initialized

---
+++ lib/homepage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:fan_carousel_image_slider/fan_carousel_image_slider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  static const List<String> sampleImages = [
+    "https://images.unsplash.com/photo-1557700836-25f2464e845d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+    "https://images.unsplash.com/photo-1669462277329-f32f928a4a79?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "https://images.unsplash.com/photo-1542840410-3092f99611a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "https://images.unsplash.com/photo-1665700301643-def92acad454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "https://images.unsplash.com/photo-1669072596436-15df4a8c083d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "https://images.unsplash.com/photo-1669178197999-32178a532277?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80",
+    "https://images.unsplash.com/photo-1539607547234-e09cdb14d473?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
+    ""
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: Column(
+        children: [
+          SizedBox(
+            height: 60,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 40,
+              ),
+              Text(
+                'Image Slider...',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 70,
+          ),
+          FanCarouselImageSlider(
+            imagesLink: sampleImages,
+            isAssets: false,
+            indicatorActiveColor: Colors.green,
+          )
+        ],
+      ),
+    );
+  }
+}
